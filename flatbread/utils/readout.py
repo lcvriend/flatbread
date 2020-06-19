52cddc09b62ce6972eda55340faafe295ddfd878
lcvriend <43742775+lcvriend@users.noreply.github.com>
2020-06-19 21:11:04 +0200

    Add timer to readout

--- flatbread/utils/readout.py
+++ flatbread/utils/readout.py
@@ -12,9 +12,12 @@ column :
     print it.
 shape :
     Print the resulting shape of the DataFrame.
+timer :
+    Measure time it takes for a function to process and print it.
 """
 
 from functools import wraps
+import time
 
 
 def printout(msg, when='before'):
@@ -51,6 +54,22 @@ def shape(table=''):
         def wrapper(*args, **kwargs):
             result = func(*args, **kwargs)
             items = [table, str(result.shape)]
+            readout = ' '.join(items)
+            print(readout)
+            return result
+        return wrapper
+    return decorator
+
+
+def timer(table=''):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            result = func(*args, **kwargs)
+            end = time.time()
+            elapsed = str(end - start)
+            items = [table, func.__name__, elapsed]
             readout = ' '.join(items)
             print(readout)
             return result
