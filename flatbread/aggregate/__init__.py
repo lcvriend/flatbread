e520d3fb40b6f614bc9ec6b228deb5d94ed098ed
lcvriend <43742775+lcvriend@users.noreply.github.com>
2020-12-31 00:27:31 +0100

    Remove unnecessary imports

--- flatbread/aggregate/__init__.py
+++ flatbread/aggregate/__init__.py
@@ -1,12 +1,6 @@
-from functools import wraps
-
 from flatbread.config import CONFIG
-from flatbread.axes import get_axis_number
 
 
 TOTALS_SETTINGS = {'aggregation': ['totals_name', 'subtotals_name']}
 PERCS_SETTINGS = {'aggregation': ['ndigits', 'label_abs', 'label_rel']}
-AGG_SETTINGS = {'aggregation':
-    TOTALS_SETTINGS['aggregation'] +
-    PERCS_SETTINGS['aggregation']
-}
+AGG_SETTINGS = {'aggregation': CONFIG['aggregation']}
