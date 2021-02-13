f27597fcf6a09a42c43a95488bfaebb6785fe733
lcvriend <43742775+lcvriend@users.noreply.github.com>
2021-02-13 20:34:18 +0100

    Add dataset builder from recipe

--- flatbread/build/test.py
+++ flatbread/build/test.py
@@ -1,4 +1,7 @@
 import random
+import re
+import string
+
 import pandas as pd
 
 
@@ -40,3 +43,60 @@ def dataset(k=100, categories=CATEGORIES, to_categories=False, seed=None):
         }
         df = df.astype(categoricals)
     return df
+
+
+def dataset_from_recipe(
+    cats_recipe,
+    k=100,
+    to_categorical=False,
+    n_chars=3
+):
+    alphabet_lower = list(string.ascii_lowercase)
+    alphabet_upper = list(string.ascii_uppercase)
+    vowels = 'aeiou'
+
+    categories = dict()
+    all_values = set()
+    for i, n_cats in enumerate(cats_recipe):
+        get_chars = lambda: random.sample(alphabet_lower, k=n_chars)
+        choices = set()
+        while len(choices) < n_cats:
+            proposed_value = ''.join(get_chars())
+            if proposed_value in all_values:
+                continue
+            if not any(char in vowels for char in proposed_value):
+                continue
+            choices.add(proposed_value)
+        all_values.update(choices)
+        if isinstance(n_cats, float):
+            choices.add(None)
+        categories[alphabet_upper[i]] = choices
+
+    populations = {
+        key:{item:random.randint(1,40) for item in items}
+        for key, items in categories.items()
+    }
+    data = {key:random.choices(
+        list(pop.keys()),
+        weights=list(pop.values()),
+        k=k) for key, pop in populations.items()
+    }
+    data.update(dict(num=[random.randint(5, 200) for i in range(k)]))
+    df = pd.DataFrame.from_dict(data)
+    if to_categorical:
+        categoricals = {
+            key:pd.CategoricalDtype(
+                [val for val in values if val is not None],
+                ordered=True,
+            )
+            for key, values in categories.items()
+        }
+        df = df.astype(categoricals)
+    return df
+
+
+def extract_part(html, part):
+    html = re.search(f"<{part}.*>[\w\W]*</{part}>", html).group()
+    html = re.sub("\s{2,}", '\n', html)
+    print(html)
+    return None
