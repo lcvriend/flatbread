866531d9678f0d6d6fd3f41627a46f67c0bb1e89
lcvriend <43742775+lcvriend@users.noreply.github.com>
2021-02-13 20:41:00 +0100

    Housekeeping

--- flatbread/aggregate/__init__.py
+++ flatbread/aggregate/__init__.py
@@ -3,4 +3,4 @@ from flatbread.config import CONFIG
 
 TOTALS_SETTINGS = {'aggregation': ['totals_name', 'subtotals_name']}
 PERCS_SETTINGS = {'aggregation': ['ndigits', 'label_abs', 'label_rel']}
-AGG_SETTINGS = {'aggregation': CONFIG['aggregation']}
+AGG_SETTINGS = 'aggregation'
