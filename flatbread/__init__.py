542cee7298d4817fd1ee27a499161e5285cff0dd
lcvriend <43742775+lcvriend@users.noreply.github.com>
2021-02-15 23:10:14 +0100

    Start with docstring for library

--- flatbread/__init__.py
+++ flatbread/__init__.py
@@ -1,7 +1,28 @@
-"""Flatbread Library
-=================
+"""Flatbread
+=========
 
-The Flatbread Library provides a collection of modules for presenting data.
+Pivot tables are a great tool for exploring and presenting your data. Flatbread
+extends pandas' pivot table capabilities by adding the `pita` accessor to your
+dataframes.
+
+## Styling
+The pivot tables viewed through flatbread have their own (customizable)
+styling. This makes the view on your data clearly distinguishable from the data itself and can add to the table's readability.
+
+Flatbread's styling uses pandas' Styler. The `style` method will return the
+Styler object which then can be used to further style the table, f.e. using the
+builtins.
+
+## Aggregates
+Flatbread makes it easy to add totals and subtotals to any axis and level of
+your table. You can also transform your table into percentages or add them into
+the table next to your data.
+
+## NA
+Pandas' pivot table only includes records if all groupings have a value. It can
+be convenient to see the NA's in your pivoted data or at least have them
+factor into the aggregate calculations. Flatbread offers the option to keep the
+NA's and either hide or show them in the final output.
 """
 
 import flatbread.pivot
@@ -17,7 +38,7 @@ import flatbread.build.rows as rows
 import flatbread.build.rows as index
 import flatbread.build.rows as idx
 import flatbread.build.test as test
-import flatbread.format as format
+import flatbread.style as style
 from flatbread.config import CONFIG
 from flatbread.plot.trendline import TrendLine
 from flatbread.utils import sanity, readout, log
