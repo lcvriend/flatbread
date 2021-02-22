5f4715ee7a8c1390ef43e055a518201b5cef8178
lcvriend <43742775+lcvriend@users.noreply.github.com>
2021-02-22 17:22:04 +0100

    Separate test table builders numbers and cats

--- flatbread/build/test.py
+++ flatbread/build/test.py
@@ -45,19 +45,35 @@ def dataset(k=100, categories=CATEGORIES, to_categories=False, seed=None):
     return df
 
 
-def dataset_from_recipe(
-    cats_recipe,
+def dataset_categoricals(
+    recipe,
     k=100,
     to_categorical=False,
     n_chars=3
 ):
+    """
+    Build a test dataset containing categorical data.
+
+    Arguments
+    ---------
+    recipe : list of int/float
+        Each element in the list represents a column.
+        The value represents the number of categories.
+        If the value is a float, then it will have missing values.
+    k : int, default 100
+        Number of rows to create.
+    to_categorical : bool, default False
+        Whether to convert the columns to categorical dtype.
+    n_chars : int, default 3
+        The number of characters the values should get.
+    """
     alphabet_lower = list(string.ascii_lowercase)
     alphabet_upper = list(string.ascii_uppercase)
     vowels = 'aeiou'
 
     categories = dict()
     all_values = set()
-    for i, n_cats in enumerate(cats_recipe):
+    for i, n_cats in enumerate(recipe):
         get_chars = lambda: random.sample(alphabet_lower, k=n_chars)
         choices = set()
         while len(choices) < n_cats:
@@ -81,7 +97,6 @@ def dataset_from_recipe(
         weights=list(pop.values()),
         k=k) for key, pop in populations.items()
     }
-    data.update(dict(num=[random.randint(5, 200) for i in range(k)]))
     df = pd.DataFrame.from_dict(data)
     if to_categorical:
         categoricals = {
@@ -93,6 +108,32 @@ def dataset_from_recipe(
         }
         df = df.astype(categoricals)
     return df
+
+
+def dataset_numbers(recipe, k=100):
+    """
+    Build a test dataset containg floats and/or integers.
+
+    Arguments
+    ---------
+    recipe : list of int/float
+        Each element in the list represents a column.
+        The value represents the number range.
+        If the value is a float, then the output will be floats.
+        If the value is an int, then the output will be integers.
+    k : int, default 100
+        Number of rows to create.
+    """
+    data = dict()
+    ints = [i for i in recipe if isinstance(i, int)]
+    floats = [i for i in recipe if isinstance(i, float)]
+
+    for i, val in enumerate(ints):
+        data[f'int{i}'] = [random.randint(0, val) for i in range(k)]
+
+    for i, val in enumerate(floats):
+        data[f'float{i}'] = [random.random() * val for i in range(k)]
+    return pd.DataFrame(data)
 
 
 def extract_part(html, part):
