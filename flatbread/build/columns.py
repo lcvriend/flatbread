47faac669a6940c4f071f475603c418b6e49a341
lcvriend <43742775+lcvriend@users.noreply.github.com>
2021-04-17 15:04:58 +0200

    Add ts_offset for aligning dates to reference year

--- flatbread/build/columns.py
+++ flatbread/build/columns.py
@@ -67,3 +67,42 @@ def pct(
     if add:
         return percs.add(df, axis=1, level=level, **kwargs)
     return percs.transform(df, axis=1, level=level, **kwargs)
+
+
+def ts_offset(
+    df : pd.DataFrame,
+    datefield,
+    ref_year : int,
+    adm_year_field=None,
+    to_column=None,
+) -> pd.DataFrame:
+    """
+    Offset dates of the ``datefield`` column in ``df`` so that they align with
+    the ``ref_year``. This can be useful when comparing multiple years to each
+    other or when plotting data.
+    
+    If ``datefield`` follows an administrative year, use ``adm_year_field`` to
+    specify the column used to calculate the necessary offset.
+
+    Arguments
+    ---------
+    df : pd.DataFrame
+        Target table.
+    datefield : scalar
+        Name of target date column.
+    ref_year : int
+        Reference year to which the dates will be offset.
+    adm_year_field : scalar, optional
+        Name of column containing administrative year if applicable.
+        If left empty the offset will be calculated on the datefield year.
+    to_column : scalar, optional
+        Name of column for the offset dates.
+        If left empty ``datefield`` will be overwritten.
+    """
+    offset = lambda g: g + pd.DateOffset(years=ref_year-g.name)
+    has_adm_year = adm_year_field is not None
+    grouper = adm_year_field if has_adm_year else df.datefield.dt.year
+    s = df.groupby(grouper)[datefield].apply(offset)
+    to_column = datefield if to_column is None else to_column
+    df[to_column] = s
+    return df
