5c5f857250928f978d3b11a0311e33fdb9f1eb71
lcvriend <43742775+lcvriend@users.noreply.github.com>
2024-11-13 23:17:06 +0100

    Add "add_level" function to add level to an index of a df or series

--- flatbread/tooling.py
+++ flatbread/tooling.py
@@ -1,3 +1,4 @@
+from functools import singledispatch
 from typing import Any, Literal, TypeAlias
 
 import pandas as pd
@@ -80,3 +81,136 @@ def sort_totals(
         **kwargs,
     )
     return output
+
+
+@singledispatch
+def add_level(
+    data,
+    item: Any,
+    level: int = 0,
+    level_name: Any = None,
+    axis: Axis = 0,
+):
+    raise NotImplementedError('No implementation for this type')
+
+
+@add_level.register
+def _(
+    data: pd.DataFrame,
+    value: Any,
+    level: int = 0,
+    level_name: Any = None,
+    axis: Axis = 0,
+) -> pd.DataFrame:
+    """
+    Add a level containing the specified value to a DataFrame axis.
+
+    Parameters
+    ----------
+    data (pd.DataFrame):
+        Input DataFrame.
+    value (Any):
+        Value to fill the new level with.
+    level (int, optional):
+        Position to insert the new level. Defaults to 0 (start).
+    level_name (Any, optional):
+        Name for the new level. Defaults to None.
+    axis (Axis):
+        Axis to modify (0 for index, 1 for columns). Defaults to 0.
+
+    Returns
+    -------
+    pd.DataFrame:
+        DataFrame with the new level added to the specified axis.
+    """
+    data = data.copy()
+    target = data.index if axis in [0, 'index'] else data.columns
+
+    if not isinstance(target, pd.MultiIndex):
+        original_name = target.name
+        target = pd.MultiIndex.from_arrays([target], names=[original_name])
+
+    new_keys = [add_value_to_key(key, value, level) for key in target]
+    new_names = add_value_to_key(target.names, level_name, level)
+    new_index = pd.MultiIndex.from_tuples(new_keys, names=new_names)
+
+    if axis in [0, 'index']:
+        data.index = new_index
+    else:
+        data.columns = new_index
+    return data
+
+
+@add_level.register
+def _(
+    data: pd.Series,
+    value: Any,
+    level: int = 0,
+    level_name: Any = None,
+    axis: Axis = 0,
+) -> pd.DataFrame:
+    """
+    Add a level containing the specified value to a Series index.
+
+    Parameters
+    ----------
+    data (pd.Series):
+        Input Series.
+    value (Any):
+        Value to fill the new level with.
+    level (int, optional):
+        Position to insert the new level. Defaults to 0 (start).
+    level_name (Any, optional):
+        Name for the new level. Defaults to None.
+    axis (Axis):
+        Added for symmetry with DataFrame method.
+
+    Returns
+    -------
+    pd.Series:
+        Series with the new level added to the specified axis.
+    """
+    data = data.copy()
+    target = data.index
+
+    if not isinstance(target, pd.MultiIndex):
+        original_name = target.name
+        target = pd.MultiIndex.from_arrays([target], names=[original_name])
+
+    new_keys = [add_value_to_key(key, value, level) for key in target]
+    new_names = add_value_to_key(target.names, level_name, level)
+    new_index = pd.MultiIndex.from_tuples(new_keys, names=new_names)
+    data.index = new_index
+    return data
+
+
+def add_value_to_key(
+    key: Any|tuple[Any, ...],
+    value: Any,
+    level: int = 0,
+) -> tuple[Any, ...]:
+    """
+    Insert an value into a key (tuple or single value refering to a column position) at a specified level.
+
+    Parameters
+    ----------
+    key (Any|tuple[Any, ...]):
+        Original key.
+    value (Any):
+        Item to insert into the key.
+    level (int, optional):
+        Position to insert the value. Defaults to 0.
+
+    Returns
+    -------
+    Tuple[Any, ...]:
+        New key with the value inserted.
+    """
+    key = list(key) if isinstance(key, (tuple, list)) else [key]
+    if level >= 0:
+        key.insert(level, value)
+    elif level == -1:
+        key.append(value)
+    else:
+        key.insert(level + 1, value)
+    return tuple(key)
