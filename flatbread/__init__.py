3cd8511a384945cdfe137f2e69a6803c10944f19
lcvriend <43742775+lcvriend@users.noreply.github.com>
2025-06-08 18:05:35 +0200

    Improve IDE autocomplete and type hints for pandas accessors
    - Add TYPE_CHECKING blocks to try and enable IDE support for .pita accessor
    - Somewhat improves autocomplete, method signatures, and docstring display

--- flatbread/__init__.py
+++ flatbread/__init__.py
@@ -1,3 +1,16 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import pandas as pd
+    from flatbread.accessors.dataframe import PitaFrame
+    from flatbread.accessors.series import PitaSeries
+
+    class DataFrame(pd.DataFrame):
+        pita: PitaFrame
+
+    class Series(pd.Series):
+        pita: PitaSeries
+
 from flatbread.config import read_config
 
 DEFAULTS = read_config()
